ccb12b19d0f70a15446b1235ce74f9cb93d40f60
songkwwwwwww <songkwwwwwww@users.noreply.github.com>
2016-07-16 11:45:11 +0900

    Add files via upload

---
+++ algorithm/bit-operation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+bool getBit(int num, int i){
+	return ((num & (1 << i)) != 0 );
+}
+
+int setBit(int num, int i){
+	return num | (1 << i);
+}
+
+int clearBit(int num, int i){
+	int mask = ~(1 << i);
+	return num & mask;
+}
+
+int clearBitsMSBthroughI(int num, int i){
+	int mask = (1 << (i + 1)) - 1;
+	return num & mask;
+}
+
+int main(){
+
+}
