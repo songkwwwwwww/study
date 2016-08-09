57582f9681b3aa16c8ad9d5bd2a4481bc531f701
Kiwon Song <songkwwwwwww@naver.com>
2016-08-10 08:05:21 +0900

    update boj dp

--- baekjoon/dynamic_programming/5721.cpp
+++ baekjoon/dynamic_programming/5721.cpp
@@ -1,8 +1,44 @@
 #include <cstdio>
 #include <cstring>
 
-#define MAX(a, b) (a) > (b) ? (a) : (b)
+#define MAX(a, b) ((a) > (b)) ? (a) : (b)
 
+//int arr[100001];
+//int dp[100001];
+
+
+int main(){
+	freopen("5721.txt", "r", stdin);
+	int m, n;
+	scanf("%d %d", &m, &n);
+	while(m && n){
+	//	memset(dp, -1, sizeof(dp));
+		int arr[100001];
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++){
+				scanf("%d", &arr[i * n + j]);
+			}
+		}
+		int dp[100001];
+		memcpy(dp, arr, sizeof(arr));
+
+		for(int i = 0; i < m; i++){ // all rows
+			for(int j = 2; j < n; j++){
+				dp[i * n + j] = MAX(dp[i * n + j-1], dp[i * n + j-2] + arr[i * n + j] );
+			} 
+		}
+
+		for(int i = 2; i < m; i++){
+			dp[(i * n) + n-1] = MAX(dp[(i-1) * n + n-1] , dp[(i-2) * n + n-1] + dp[(i * n) + n-1] );
+		}
+
+		printf("%d\n", dp[(m-1)*n + n-1]);
+		scanf("%d %d", &m, &n);
+	}
+}
+
+
+/*
 int m, n; // 1<= m * n <= 10,000
 
 int arr[10000];
@@ -54,3 +90,5 @@ int main(){
 		scanf("%d %d", &m, &n);
 	}
 }
+
+*/
