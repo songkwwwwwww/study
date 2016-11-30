168dbf95a1a69660f26670580e5f853072f4d501
Kiwon Song <songkwwwwwww@naver.com>
2016-11-30 10:17:42 +0900

    update

--- apue/3-11.c
+++ apue/3-11.c
@@ -3,12 +3,11 @@
 
 int main(int argc, char* argv[]){
     int val;
-    
     if(argc != 2)
-	err_quit("usage: a.out <descriptor#>");
+		err_quit("usage: a.out <descriptor#>");
 
     if((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
-	err_sys("fcntl error for fd %d", atoi(argv[1]));
+		err_sys("fcntl error for fd %d", atoi(argv[1]));
 
     switch(val & O_ACCMODE){
 	case O_RDONLY:
@@ -24,11 +23,11 @@ int main(int argc, char* argv[]){
 	    err_dump("unknown access mode");
     }
     if(val & O_APPEND)
-	printf(", append");
+		printf(", append");
     if(val & O_NONBLOCK)
-	printf(", nonblocking");
+		printf(", nonblocking");
     if(val & O_SYNC)
-	printf(", synchronous writes");
+		printf(", synchronous writes");
 #if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC) && (O_FSYNC != O_SYNC)
     if(val & O_FSYNC)
 	printf(", synchronous writes");
